7a756619abb2673cc7b106eb89e34aa85468e7f2
NickyGuants <nicholasguantai528@gmail.com>
2021-06-28 16:46:32 +0300

    add the bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:dsc_chuka/screens/home_screen.dart';
+import 'package:dsc_chuka/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,18 +7,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      color: Colors.white,
-      title: 'DSC Chuka university',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.purple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(),
-      routes: {},
-    );
+        title: 'DSC Chuka University',
+        theme: ThemeData(
+          // This is the theme of your application.
+          primarySwatch: Colors.blueGrey,
+        ),
+        home: MainScreen());
   }
 }
